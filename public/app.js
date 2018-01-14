71da0a2253707ca60e00d8b569c0fbb319b83f55
omes000 <omes000@gmail.com>
2018-01-13 18:14:35 -0600

    adding notes

--- public/app.js
+++ public/app.js
@@ -19,9 +19,8 @@ function displayArticles(){
 function savedArticles () {
 	$.get("/api/saved").then((data) => {
 		console.log(data);
-
 		for(var i = 0; i < data.length; i++){
-			$("#saved-articles").append("<div class='panel panel-default' data-id='"+ data[i]._id +"'><div class='panel-heading'><h3 class='panel-title'>" + data[i].title + "</h3><button type = 'button' class = 'btn btn-default pull-right remove-article' id ='" + data[i]._id +"'>Remove Article</button></div><div class='panel-body'><a href ='" + data[i].link + "'>"+ data[i].link +"</a></div></div>")
+			$("#saved-articles").append("<div class='panel panel-default' data-id='"+ data[i]._id +"'><div class='panel-heading'><h3 class='panel-title'>" + data[i].title + "</h3><button type = 'button' class = 'btn btn-default pull-right remove-article' id ='" + data[i]._id +"'>Remove Article</button><button type = 'button' class = 'btn btn-default pull-right comment-article' id ='" + data[i]._id +"'>Comment</button></div><div class='panel-body'><a href ='" + data[i].link + "'>"+ data[i].link +"</a><div id = 'notes'></div></div></div>")
 		}
 	})
 }
@@ -57,11 +56,11 @@ $(document).on("click", ".save-article", function(){
 	})
 })
 
-$(document).on("click", "p", function() {
+$(document).on("click", ".comment-article", function() {
 
 	$("#notes").empty();
 
-	var thisId = $(this).attr("data-id");
+	var thisId = $(this).attr("id");
 
 	$.ajax({
 		method: "GET",
@@ -77,7 +76,7 @@ $(document).on("click", "p", function() {
 			// A textarea to add a new note body
 			$("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
 			// A button to submit a new note, with the id of the article saved to it
-			$("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+			$("#notes").append("<button class = 'btn btn-default' data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
 			// If there's a note in the article
 			if (data.note) {
@@ -92,28 +91,29 @@ $(document).on("click", "p", function() {
 // When you click the savenote button
 $(document).on("click", "#savenote", function() {
 // Grab the id associated with the article from the submit button
-var thisId = $(this).attr("data-id");
+	var thisId = $(this).attr("data-id");
+	console.log("saving")
 
-// Run a POST request to change the note, using what's entered in the inputs
-$.ajax({
-method: "POST",
-url: "/articles/" + thisId,
-data: {
-// Value taken from title input
-title: $("#titleinput").val(),
-// Value taken from note textarea
-body: $("#bodyinput").val()
-}
-})
-// With that done
-.done(function(data) {
-// Log the response
-console.log(data);
-// Empty the notes section
-$("#notes").empty();
-});
+	// Run a POST request to change the note, using what's entered in the inputs
+	$.ajax({
+	method: "POST",
+	url: "/articles/" + thisId,
+	data: {
+	// Value taken from title input
+	title: $("#titleinput").val(),
+	// Value taken from note textarea
+	body: $("#bodyinput").val()
+	}
+	})
+	// With that done
+	.done(function(data) {
+	// Log the response
+	console.log(data);
+	// Empty the notes section
+	$("#notes").empty();
+	});
 
-// Also, remove the values entered in the input and textarea for note entry
-$("#titleinput").val("");
-$("#bodyinput").val("");
+	// Also, remove the values entered in the input and textarea for note entry
+	$("#titleinput").val("");
+	$("#bodyinput").val("");
 });
